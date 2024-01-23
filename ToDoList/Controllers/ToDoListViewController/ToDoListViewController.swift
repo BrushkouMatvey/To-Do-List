e8d542930ecaf843160f4f060932ba7c62334ee5
MatveyBrushkou <mbrushkou@gmail.com>
2024-01-23 10:13:54 +0300

    up: add new items button

--- ToDoList/Controllers/ToDoListViewController/ToDoListViewController.swift
+++ ToDoList/Controllers/ToDoListViewController/ToDoListViewController.swift
@@ -18,5 +18,31 @@ class ToDoListViewController: UITableViewController {
     
         items = saveLoadService.loadArray()
     }
+    
+    @IBAction func onAddButtonClicked(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        var alertController = UIAlertController(title: "Add new Item", message: "You can add new ToDo Item" + "😀", preferredStyle: .alert)
+        
+        var addAlertAction = UIAlertAction(title: "Add", style: .default) { action in
+            self.addNewItem(message: textField.text!)
+        }
+        
+        alertController.addTextField { configurationTextField in
+            textField = configurationTextField
+        }
+        
+        alertController.addAction(addAlertAction)
+        present(alertController, animated: true)
+        
+    }
+    
+    
+    private func addNewItem(message: String){
+        items.append(Item(message, false))
+        saveLoadService.saveArray(items)
+        tableView.reloadData()
+    }
 }
 
